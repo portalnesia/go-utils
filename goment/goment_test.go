4a6fe6bed7ea4312e9bde933e9fe04327888dd76
Putu Aditya <aditya@portalnesia.com>
2022-09-12 17:45:31 +0700

    Change Custom Goment

--- goment/goment_test.go
+++ goment/goment_test.go
@@ -212,15 +212,366 @@ func TestRangeFormatTime(t *testing.T) {
 	if err != nil {
 		t.Errorf("[RangeFormatTime] %+v", err)
 	}
-	a.Subtract(2, "hours")
-	b, err := New()
+	b := a.Clone().Subtract(2, "hours")
+
+	r := b.RangeFormat(a)
+
+	if r != fmt.Sprintf("%s - %s, %s", b.Format("HH:mm"), a.Format("HH:mm"), b.Format("DD MMMM YYYY")) {
+		t.Errorf("[RangeFormatTime]. Get: %s", r)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	a, err := New("2022-05-05 12:00:00")
 	if err != nil {
-		t.Errorf("[RangeFormatTime] %+v", err)
+		t.Errorf("[TestAdd] %+v", err)
 	}
 
-	r := a.RangeFormat(b)
+	a.Add(6, "hours")
+	f := a.Hour()
 
-	if r != fmt.Sprintf("%s - %s, %s", a.Format("HH:mm"), b.Format("HH:mm"), a.Format("DD MMMM YYYY")) {
-		t.Errorf("[RangeFormatTime]. Get: %s", r)
+	if f != 18 {
+		t.Errorf("[TestAdd] Not added. %d", f)
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	a, err := New("2022-05-05 12:00:00")
+	if err != nil {
+		t.Errorf("[TestSubtract] %+v", err)
+	}
+
+	a.Subtract(6, "hours")
+	f := a.Hour()
+
+	if f != 6 {
+		t.Errorf("[TestSubtract] Not Subtracted. %d", f)
+	}
+}
+
+func TestClone(t *testing.T) {
+	a, err := New()
+	if err != nil {
+		t.Error(err)
+	}
+
+	b := a.Clone().Subtract(2, "hours")
+
+	if a.Format() == b.Format() {
+		t.Error()
+	}
+}
+
+func TestEndOf(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	b := a.EndOf("month")
+
+	if b.Date() != 30 {
+		t.Error()
+	}
+}
+
+func TestStartOf(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	b := a.StartOf("month")
+
+	if b.Date() != 1 {
+		t.Error()
+	}
+}
+
+func TestSetByUnits(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.Set("y", 2016).Year() != 2016 {
+		t.Error()
+	}
+	if a.Set("year", 2017).Year() != 2017 {
+		t.Error()
+	}
+	if a.Set("years", 2018).Year() != 2018 {
+		t.Error()
+	}
+	if a.Set("M", 9).Month() != 9 {
+		t.Error()
+	}
+	if a.Set("month", 10).Month() != 10 {
+		t.Error()
+	}
+	if a.Set("months", 11).Month() != 11 {
+		t.Error()
+	}
+	if a.Set("D", 8).Date() != 8 {
+		t.Error()
+	}
+	if a.Set("D", 9).Date() != 9 {
+		t.Error()
+	}
+	if a.Set("D", 10).Date() != 10 {
+		t.Error()
+	}
+	if a.Set("h", 14).Hour() != 14 {
+		t.Error()
+	}
+	if a.Set("hour", 15).Hour() != 15 {
+		t.Error()
+	}
+	if a.Set("hours", 16).Hour() != 16 {
+		t.Error()
+	}
+	if a.Set("m", 17).Minute() != 17 {
+		t.Error()
+	}
+	if a.Set("minute", 18).Minute() != 18 {
+		t.Error()
+	}
+	if a.Set("minutes", 19).Minute() != 19 {
+		t.Error()
+	}
+	if a.Set("s", 20).Second() != 20 {
+		t.Error()
+	}
+	if a.Set("second", 21).Second() != 21 {
+		t.Error()
+	}
+	if a.Set("seconds", 22).Second() != 22 {
+		t.Error()
+	}
+	if a.Set("ms", 23000).Millisecond() != 23000 {
+		t.Error()
+	}
+	if a.Set("millisecond", 24000).Millisecond() != 24000 {
+		t.Error()
+	}
+	if a.Set("milliseconds", 25000).Millisecond() != 25000 {
+		t.Error()
+	}
+	if a.Set("ns", 100000).Nanosecond() != 100000 {
+		t.Error()
+	}
+	if a.Set("nanosecond", 100001).Nanosecond() != 100001 {
+		t.Error()
+	}
+	if a.Set("nanoseconds", 100002).Nanosecond() != 100002 {
+		t.Error()
+	}
+}
+
+func TestSetDay(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Errorf("[TestSetDay] %+v", err)
+	}
+
+	a.SetDay(0)
+	f := a.Day()
+
+	if f != 0 {
+		t.Error()
+	}
+
+	a.SetDay("tuesday")
+	f = a.Day()
+
+	if f != 2 {
+		t.Error()
+	}
+}
+
+func TestSetYear(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetYear(2016).Year() != 2016 {
+		t.Error()
+	}
+}
+
+func TestSetMonth(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetMonth(5).Month() != 5 {
+		t.Error()
+	}
+}
+
+func TestSetDate(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetDate(5).Date() != 5 {
+		t.Error()
+	}
+}
+
+func TestSetDayOfYear(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetDayOfYear(300).DayOfYear() != 300 {
+		t.Error()
+	}
+}
+
+func TestSetHour(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetHour(12).Hour() != 12 {
+		t.Error()
+	}
+}
+
+func TestSetMinute(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetMinute(12).Minute() != 12 {
+		t.Error()
+	}
+}
+
+func TestSetMillisecond(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetMillisecond(1000).Millisecond() != 1000 {
+		t.Error()
+	}
+}
+
+func TestSetNanosecond(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetNanosecond(1000000).Nanosecond() != 1000000 {
+		t.Error()
+	}
+}
+
+func TestSetSecond(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetSecond(12).Second() != 12 {
+		t.Error()
+	}
+}
+
+func TestSetWeek(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetWeek(12).Week() != 12 {
+		t.Error()
+	}
+}
+
+func TestSetWeekYear(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetWeekYear(2020).WeekYear() != 2020 {
+		t.Error()
+	}
+}
+
+func TestSetWeekDay(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetWeekday(2).Weekday() != 2 {
+		t.Error()
+	}
+}
+
+func TestSetQuarter(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetQuarter(1).Quarter() != 1 {
+		t.Error()
+	}
+}
+
+func TestSetUTCOffset(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetUTCOffset(7).UTCOffset() != 7*60 {
+		t.Error()
+	}
+}
+
+func TestSetISOWeek(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetISOWeek(50).ISOWeek() != 50 {
+		t.Error()
+	}
+}
+
+func TestSetISOWeekYear(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetISOWeekYear(2022).ISOWeekYear() != 2022 {
+		t.Error()
+	}
+}
+
+func TestSetISOWeekday(t *testing.T) {
+	a, err := New("2022-09-12 12:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.SetISOWeekday(2).ISOWeekday() != 2 {
+		t.Error()
 	}
 }
